3eb35249f657d23bf29a6bcf836917080b55b696
jz393 <jane.zhangjiaye@gmail.com>
2019-01-03 13:24:18 -0600

    Initial Commit

---
+++ test_cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test_cpp
+//
+//  Created by Jane Zhang on 1/3/19.
+//  Copyright © 2019 Jane Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

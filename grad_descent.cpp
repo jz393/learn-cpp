7ef6a15c8a8c3e4ba56e6f1e93e2d40f687dcd2c
jz393 <jane.zhangjiaye@gmail.com>
2019-01-07 17:35:41 -0600

    new file

---
+++ grad_descent.cpp
@@ -0,0 +1,10 @@
+//  Created by Jane Zhang on 1/7/19.
+//  Copyright © 2019 Jane Zhang. All rights reserved.
+/*  This is a simple implementation of the gradient 
+descent algorithm commonly used in machine learning,
+running on a linear regression model.
+
+y = ax + b
+e = predicted - actual
+*/
+

b0bb8eaf2e546d440052daa935101ea20a8a27b4
jz393 <jane.zhangjiaye@gmail.com>
2019-01-08 22:40:37 -0600

    heap vs. stack

--- structs.cpp
+++ structs.cpp
@@ -40,20 +40,29 @@ TreeNode* search(TreeNode* root, int val) {
 int main() {
     //making tree of 8 -> 7, 9
 
+    //on stack
     TreeNode tree1 = TreeNode(8); //struct
     TreeNode * tree1_ptr = &tree1; //struct pointer...& finds address
 
-    TreeNode tree2 = TreeNode(7);
-    TreeNode * tree2_ptr = &tree2;
+    //on heap..this is essentially malloc
+    // TreeNode tree2 = TreeNode(7);
+    // TreeNode * tree2_ptr = &tree2;
+    TreeNode * tree2_ptr = new TreeNode(7); //can also alloc like this
 
+    //on stack
     TreeNode tree3 = TreeNode(9);
     TreeNode * tree3_ptr = &tree3;
 
+    //creating tree
     tree1_ptr->left = tree2_ptr;
     tree1_ptr->right = tree3_ptr;
 
     TreeNode* source = search(tree1_ptr, 7);
     cout << source->val << endl;
+
+    //free the pointer (only tree2 was allocated)..the stack ones auto deleted
+    //delete tree1_ptr; --> does not work bc was on stack
+    delete tree2_ptr;
 
     return 0;
 }
